e9fd8adcc4f926b69b325f8a32bcc2a0ab648f6b
David Rotert <d.rotert@outlook.de>
2025-11-01 22:08:01 +0100

    Use simple overwrite of values in RaycastingSystem
    Signed-off-by: David Rotert <d.rotert@outlook.de>

--- src/RaycastingSystem.cpp
+++ src/RaycastingSystem.cpp
@@ -6,15 +6,11 @@ RaycastingSystem::RaycastingSystem(const int horizontalResolution, const Player&
     horizontalResolution(horizontalResolution),
     player(player),
     map(map),
-    rayCastResults(std::vector<RayCastResult>())
-{
-    this->rayCastResults.reserve(horizontalResolution);
-}
+    rayCastResults(std::vector(this->horizontalResolution, RayCastResult{Vector2{0, 0}, 0, 0}))
+{}
 
 void RaycastingSystem::calculateRays()
 {
-    this->rayCastResults.clear();
-
     radian absoluteStartAngle = absolute_radian(this->player.rotationAngleRadian - (this->player.fov / 2));
     const radian anglePerRay = (this->player.fov / this->horizontalResolution);
 
@@ -28,7 +24,7 @@ void RaycastingSystem::calculateRays()
             this->player.getRotationAsNonNormalizedVector(currentAbsoluteAngle)
         );
         rayCastResult.rayLength = std::cos(currentAngleRelativeToCamera) * rayCastResult.rayLength;
-        this->rayCastResults.push_back(rayCastResult);
+        this->rayCastResults.at(i) = rayCastResult;
     }
 }
 
